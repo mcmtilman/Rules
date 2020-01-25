2682b198b885e473259875f0646f0e0ada42a9b4
Michel Tilman <mtilman@acm.org>
2020-01-25 13:20:22 +0100

    Added support for rule sets with initial tests

--- Sources/Rules/Model/Rules.swift
+++ Sources/Rules/Model/Rules.swift
@@ -7,17 +7,101 @@
 //  Licensed under Apache License v2.0.
 //
 
-public protocol Rule: Expression {}
+/**
+ Superclass for rules.
+ A rule evalutes in a given context to true or false.
+ The rule execution may depend on a pre-condition (the rule must *match* the context).
+ If the rule pre-condition does not match the context, the result is nil.
+*/
+public class Rule {
+    
+    /// Answer the result of evaluating the rule in given context.
+    /// The result is either true or false if the rule matches, or nil if there is no match.
+    func eval<C>(in context: C) throws -> Bool? {
+        fatalError("Abstract method must be overridden")
+    }
+
+}
+
 
 /**
- A condition-assertion rule asserts that the input is valid, provided that the condition holds.
- If the condition is false, the assertion is ignored.
+ A condition-assertion rule asserts that the input is valid, provided that the condition is true (*the rule matches*).
+ If the condition is false, the assertion is not evaluated.
 */
-public class ConditionAssertionRule<A: Expression, B: Expression>: BinaryOperation<A, B>, Rule where A.Eval == Bool, B.Eval == Bool {
+public class ConditionAssertionRule<A: Expression, B: Expression>: Rule where A.Eval == Bool, B.Eval == Bool {
     
-    /// Returns the result of the assertion if the condition is true, or nil otherwise.
-    public func eval<C>(in context: C) throws -> Bool? {
-        try lhs.eval(in: context) ? try rhs.eval(in: context) : nil
+    // Assertion of the rule.
+    private let assertion: B
+
+    // Condition of the rule.
+    private let condition: A
+    
+    /// Name of the rule.
+    public let name: String
+    
+    /// Initializes the rule.
+    public init(name: String, condition: A, assertion: B) {
+        self.name = name
+        self.condition = condition
+        self.assertion = assertion
+    }
+    
+    /// Evaluates the rule in given context, according to the following strategy:
+    /// * If the rule matches, answer the result of evaluating the assertion.
+    /// * Return nil otherwise.
+    public override func eval<C>(in context: C) throws -> Bool? {
+        guard try condition.eval(in: context) else { return nil }
+        
+        return try assertion.eval(in: context)
+    }
+
+}
+
+
+/**
+ A rule set contains zero or more rules.
+ A rule set has a condition that must be true (*the rule set matches*), If false, the rules in the set are ignored.
+ As rule sets subclass Rule, rule sets may be nested.
+*/
+public class RuleSet<A: Expression>: Rule where A.Eval == Bool {
+    
+    // Condition of the rule set.
+    private let condition: A
+    
+    // Ordered set of rules.
+    private let rules: [Rule]
+    
+    // If *matchAll* is true (default), evaluation returns the AND-combination of the results of all matching rules.
+    // Otherwise, evaluation returns the result of the first matching rule.
+    private let matchAll: Bool
+    
+    /// Name of the rule set.
+    public let name: String
+    
+    /// Initializes the rule set.
+    public init(name: String, condition: A, rules: [Rule], matchAll: Bool = true) {
+        self.name = name
+        self.condition = condition
+        self.rules = rules
+        self.matchAll = matchAll
+    }
+    
+    /// Evaluates the rule set in given context, according to the following strategy:
+    /// * If the rule set does not match or if no rule matches return nil.
+    /// * If *matchAll* is true (default), return the AND-combination of the results of all matching rules.
+    /// * Otherwise, return the result of the first matching rule.
+    public override func eval<C>(in context: C) throws -> Bool? {
+        guard try condition.eval(in: context) else { return nil }
+        var result: Bool? = nil
+        
+        for rule in rules {
+            if let eval = try rule.eval(in: context) {
+                guard matchAll, eval else { return eval }
+                result = true
+            }
+        }
+        
+        return result
     }
 
 }
--- Tests/RulesTests/Model/RuleTests.swift
+++ Tests/RulesTests/Model/RuleTests.swift
@@ -15,16 +15,45 @@ class RuleTests: XCTestCase {
     
     // MARK: Testing condition-assertion rules.
     
-    // Test if a rule with false condition evaluates to nil.
-    func testEvalFalseCondition() {
-        XCTAssertNil(try ConditionAssertionRule(false, false).eval(in: ()))
-        XCTAssertNil(try ConditionAssertionRule(false, true).eval(in: ()))
+    // Test if a non-matching rule evaluates to nil.
+    func testEvalNonMatchingCARule() {
+        XCTAssertNil(try ConditionAssertionRule(name: "Rule", condition: false, assertion: false).eval(in: ()))
+        XCTAssertNil(try ConditionAssertionRule(name: "Rule", condition: false, assertion: true).eval(in: ()))
     }
 
-    // Test if a rule with true condition evaluates to the assertion result.
-    func testEvalTrueCondition() {
-        XCTAssertFalse(try XCTUnwrap(try ConditionAssertionRule(true, false).eval(in: ())))
-        XCTAssertTrue(try XCTUnwrap(try ConditionAssertionRule(true, true).eval(in: ())))
+    // Test if a matching rule evaluates to the assertion result.
+    func testEvalMatchingCARule() {
+        XCTAssertFalse(try XCTUnwrap(try ConditionAssertionRule(name: "Rule", condition: true, assertion: false).eval(in: ())))
+        XCTAssertTrue(try XCTUnwrap(try ConditionAssertionRule(name: "Rule", condition: true, assertion: true).eval(in: ())))
+    }
+
+    // MARK: Testing rule sets.
+    
+    // Test if a non-matching ruleset evaluates to nil.
+    func testEvalNonMatchingRuleSet() {
+        let rule = ConditionAssertionRule(name: "Rule", condition: true, assertion: false)
+        let ruleSet = RuleSet(name: "Rule set", condition: false, rules: [rule])
+        
+        XCTAssertNil(try ruleSet.eval(in: ()))
+    }
+
+    // Test if a non-empty matching ruleset evaluates to nil.
+    func testEvalMatchingRuleSet() throws {
+        let rule = ConditionAssertionRule(name: "Rule", condition: true, assertion: true)
+        let ruleSet = RuleSet(name: "Rule set", condition: true, rules: [rule])
+        
+        guard let result = try ruleSet.eval(in: ()) else { return XCTFail("nill result") }
+        
+        XCTAssertTrue(result)
+    }
+
+    // Test if (matching) empty rulesets evaluate to nil.
+    func testEvalEmptyRuleSet() throws {
+        let ruleSetMatchAll = RuleSet(name: "Rule set", condition: true, rules: [ConditionAssertionRule<Bool, Bool>]())
+        let ruleSetMatchFirst = RuleSet(name: "Rule set", condition: true, rules: [ConditionAssertionRule<Bool, Bool>](), matchAll: false)
+
+        XCTAssertNil(try ruleSetMatchAll.eval(in: ()))
+        XCTAssertNil(try ruleSetMatchFirst.eval(in: ()))
     }
 
 }
@@ -36,8 +65,11 @@ class RuleTests: XCTestCase {
 extension RuleTests {
     
     static var allTests = [
-        ("testEvalFalseCondition", testEvalFalseCondition),
-        ("testEvalTrueCondition", testEvalTrueCondition),
+        ("testEvalNonMatchingCARule", testEvalNonMatchingCARule),
+        ("testEvalMatchingCARule", testEvalMatchingCARule),
+        ("testEvalNonMatchingRuleSet", testEvalNonMatchingRuleSet),
+        ("testEvalMatchingRuleSet", testEvalMatchingRuleSet),
+        ("testEvalEmptyRuleSet", testEvalEmptyRuleSet),
     ]
 
 }
