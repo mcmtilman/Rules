3297769dc253a660dd1e5716be62bf52bcc04b60
Michel Tilman <mtilman@acm.org>
2020-07-07 16:06:28 +0200

    Updated Common dependency.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Rules"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/mcmtilman/Common.git", from: "1.1.0"),
+        .package(url: "https://github.com/mcmtilman/Common.git", from: "1.1.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
